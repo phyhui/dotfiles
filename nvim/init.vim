8a7ec04ad80896bc9b81b7820752489dccf27e95
phiskrzy <phineas.berdelis@icloud.com>
2022-09-26 15:28:40 +0200

    init powershell & vscodenvim config

---
+++ nvim/init.vim
@@ -0,0 +1,7 @@
+if exists('g:vscode')
+  " VSCode extension
+ source C:\Users\phine\.config\nvim\vscode\settings.vim
+else
+   ordinary neovim
+endif
+
---
+++ powershell/user_profile.ps1
@@ -0,0 +1,31 @@
+#Prompt
+oh-my-posh --init --shell pwsh --config "C:\Users\phine\.config\powershell\phine.omp.json" | Invoke-Expression
+
+#Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadLineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tip 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+ Get-Command -Name $command -ErrorAction SilentlyContinue | 
+  Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
